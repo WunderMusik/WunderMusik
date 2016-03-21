520c1e90e46cccaaa059eb4cdf08aaa0190c966c
Eugene <eugene.07@list.ru>
2016-03-21 23:28:16 +0300

    #2 Moved drawables to project

--- app/src/main/java/ru/bmstu/wundermusik/MainActivity.java
+++ app/src/main/java/ru/bmstu/wundermusik/MainActivity.java
@@ -1,5 +1,8 @@
 package ru.bmstu.wundermusik;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
