e9ce72d1e7241a932f7c3a5956875a763e50554e
Eugene <eugene.07@list.ru>
2016-03-21 23:28:16 +0300

    #2 Moved drawables to project

--- app/src/main/java/ru/bmstu/wundermusik/MainActivity.java
+++ app/src/main/java/ru/bmstu/wundermusik/MainActivity.java
@@ -1,19 +1,15 @@
 package ru.bmstu.wundermusik;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
 import ru.bmstu.wundermusik.fragments.PlayerFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PlayerFragment.PlayerActionsListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +43,10 @@ public class MainActivity extends AppCompatActivity {
 
             });
         }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
