16922e79a54b15ceb92409cc984a8b8647a130e3
NFetissow <nfetissow@gmail.com>
2016-04-18 23:03:47 +0300

    Trying to test it
    Added garbage code to launch streaming

--- app/src/main/java/ru/bmstu/wundermusik/MusicPlayer/MusicPlayer.java
+++ app/src/main/java/ru/bmstu/wundermusik/MusicPlayer/MusicPlayer.java
@@ -1,14 +1,33 @@
 package ru.bmstu.wundermusik.MusicPlayer;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 
+import java.io.IOException;
 import java.util.List;
 
 import ru.bmstu.wundermusik.models.Track;
 
-public class MusicPlayer implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
+public class MusicPlayer extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener, AudioManager.OnAudioFocusChangeListener {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+
+    }
+
     public enum PlayerState {
         NOT_INITIALISED,
+        IDLE,
         STOPPED,
         PAUSED,
         PLAYING,
@@ -17,16 +36,20 @@ public class MusicPlayer implements MediaPlayer.OnCompletionListener, MediaPlaye
     private MediaPlayer mediaPlayer = null;
     private List<Track> trackList = null;
     private Integer currentTrackPos = null;
-    private PlayerState playerState = PlayerState.NOT_INITIALISED;
+    private PlayerState playerState = PlayerState.IDLE;
     private void setUpMediaPlayer()
     {
         if(this.mediaPlayer == null)
         {
             this.mediaPlayer = new MediaPlayer();
-            this.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            this.mediaPlayer.setOnCompletionListener(this);
-            //TODO: set up player properties, e.g. WakeMode
+
+
         }
+        //TODO: set up player properties, e.g. WakeMode
+        this.mediaPlayer.reset();
+        this.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        this.mediaPlayer.setOnPreparedListener(this);
+        this.mediaPlayer.setOnCompletionListener(this);
     }
 
     public void onPlayRequest()
@@ -54,6 +77,23 @@ public class MusicPlayer implements MediaPlayer.OnCompletionListener, MediaPlaye
 
     }
 
+    public void playSongAt(int index) throws IOException
+    {
+        if(this.playerState == PlayerState.NOT_INITIALISED)
+        {
+            //TODO: exception, fail
+        }
+        if(index > trackList.size() - 1)
+        {
+            //TODO: OUT OF RANGE
+        }
+        setUpMediaPlayer();
+        currentTrackPos = index;
+        mediaPlayer.setDataSource(trackList.get(currentTrackPos).getStreamUrl());
+        changePlayerState(PlayerState.PREPARING);
+        mediaPlayer.prepareAsync();
+    }
+
     private void changePlayerState(PlayerState newState)
     {
         //TODO: notify about changed state
@@ -68,20 +108,28 @@ public class MusicPlayer implements MediaPlayer.OnCompletionListener, MediaPlaye
 
 
     private void startPlayng() {
-        if(!mediaPlayer.isPlaying())
-        {
-            changePlayerState(PlayerState.PLAYING);
-            mediaPlayer.start();
-        }
+        changePlayerState(PlayerState.PLAYING);
+
+        mediaPlayer.start();
     }
 
     @Override
     public void onCompletion(MediaPlayer mp) {
+        if(currentTrackPos < trackList.size() - 1)
+        {
+            try{
+            playSongAt(currentTrackPos+1);
 
+                } catch (Exception e)
+            {
+
+            }
+        }
     }
 
     @Override
     public void onPrepared(MediaPlayer mp) {
+
         startPlayng();
     }
 
