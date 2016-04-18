f9c4d43531929df1bc4a065e65936af58a7832cf
NFetissow <nfetissow@gmail.com>
2016-04-19 01:23:46 +0300

    Another error in JSON string for SIngerTest

--- app/src/test/java/ru/bmstu/wundermusik/SingerJSONParseTest.java
+++ app/src/test/java/ru/bmstu/wundermusik/SingerJSONParseTest.java
@@ -15,7 +15,7 @@ import ru.bmstu.wundermusik.models.parsers.*;
  * Created by Nikita on 4/18/2016.
  */
 public class SingerJSONParseTest {
-    private final String SINGLE_SINGER_JSON = "{\"id\": 1,  \"username\": \"Singer name\",  \"avatar_url\": \"http://www.avatar.url.com/avatar.png\"}";
+    private final String SINGLE_SINGER_JSON = "{\"id\": 359,  \"username\": \"Singer name\",  \"avatar_url\": \"http://www.avatar.url.com/avatar.png\"}";
     private final String SINGLE_SINGER_AVATAR_URL ="http://www.avatar.url.com/avatar.png";
     private final long SINGLE_SINGER_ID = 359;
     private final String SINGLE_SINGER_NAME ="Singer name";
