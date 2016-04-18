7211863e4812ed8f89aa34b754c25d21c6d111c2
NFetissow <nfetissow@gmail.com>
2016-04-19 00:34:17 +0300

    SingleSingerTest polish

--- app/src/test/java/ru/bmstu/wundermusik/SingerJSONParseTest.java
+++ app/src/test/java/ru/bmstu/wundermusik/SingerJSONParseTest.java
@@ -15,11 +15,7 @@ import ru.bmstu.wundermusik.models.parsers.*;
  * Created by Nikita on 4/18/2016.
  */
 public class SingerJSONParseTest {
-    private final String SINGLE_SINGER_JSON = "{\n" +
-            "  \"id\": \"359\",\n" +
-            "  \"username\": \"Singer name\",\n" +
-            "  \"avatar_url\": \"http://www.avatar.url.com/avatar.png\"\n" +
-            "}";
+    private final String SINGLE_SINGER_JSON = "{\"id\": \"1\",  \"username\": \"Singer name\",  \"avatar_url\": \"http://www.avatar.url.com/avatar.png\"}";
     private final String SINGLE_SINGER_AVATAR_URL ="Singer name";
     private final long SINGLE_SINGER_ID = 359;
     private final String SINGLE_SINGER_NAME ="http://www.avatar.url.com/avatar.png";
@@ -28,10 +24,8 @@ public class SingerJSONParseTest {
     public void CreateNewSingerTest() {
         JsonParser<Singer> singerJsonParser = new SingerJsonParser();
         Singer singer = singerJsonParser.parseSingleObject(SINGLE_SINGER_JSON);
-        assertEquals(singer.getAvatarUrl(), SINGLE_SINGER_AVATAR_URL);
-        assertEquals(singer.getId(), SINGLE_SINGER_ID);
-        assertEquals(singer.getName(), SINGLE_SINGER_NAME);
+        assertEquals(SINGLE_SINGER_AVATAR_URL, singer.getAvatarUrl());
+        assertEquals(SINGLE_SINGER_ID, singer.getId());
+        assertEquals(SINGLE_SINGER_NAME, singer.getName());
     }
-
-
 }
