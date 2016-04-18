5615eadc0462ebc85fd91675b87ad8dfd96f1e51
NFetissow <nfetissow@gmail.com>
2016-04-19 01:16:21 +0300

    TrackPasrer UnitTest, SingerParserTest tweaks
    Added TrackParserTest,
    Added "Assert not null" in SingerTest, removed "" from ID in json
    string.

--- app/src/test/java/ru/bmstu/wundermusik/SingerJSONParseTest.java
+++ app/src/test/java/ru/bmstu/wundermusik/SingerJSONParseTest.java
@@ -15,7 +15,7 @@ import ru.bmstu.wundermusik.models.parsers.*;
  * Created by Nikita on 4/18/2016.
  */
 public class SingerJSONParseTest {
-    private final String SINGLE_SINGER_JSON = "{\"id\": \"1\",  \"username\": \"Singer name\",  \"avatar_url\": \"http://www.avatar.url.com/avatar.png\"}";
+    private final String SINGLE_SINGER_JSON = "{\"id\": 1,  \"username\": \"Singer name\",  \"avatar_url\": \"http://www.avatar.url.com/avatar.png\"}";
     private final String SINGLE_SINGER_AVATAR_URL ="http://www.avatar.url.com/avatar.png";
     private final long SINGLE_SINGER_ID = 359;
     private final String SINGLE_SINGER_NAME ="Singer name";
@@ -24,6 +24,7 @@ public class SingerJSONParseTest {
     public void CreateNewSingerTest() {
         JsonParser<Singer> singerJsonParser = new SingerJsonParser();
         Singer singer = singerJsonParser.parseSingleObject(SINGLE_SINGER_JSON);
+        assertNotNull(singer);
         assertEquals(SINGLE_SINGER_AVATAR_URL, singer.getAvatarUrl());
         assertEquals(SINGLE_SINGER_ID, singer.getId());
         assertEquals(SINGLE_SINGER_NAME, singer.getName());
