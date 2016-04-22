88ca0877e72438238cd998d02889c8570b9956a8
NFetissow <nfetissow@gmail.com>
2016-04-22 20:53:18 +0300

    Dumb polishing

--- app/src/main/java/ru/bmstu/wundermusik/MusicPlayer/MusicPlayer.java
+++ app/src/main/java/ru/bmstu/wundermusik/MusicPlayer/MusicPlayer.java
@@ -1,28 +1,17 @@
 package ru.bmstu.wundermusik.MusicPlayer;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
-
+import android.os.PowerManager;
 import java.io.IOException;
 import java.util.List;
 
 import ru.bmstu.wundermusik.models.Track;
 
-public class MusicPlayer extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener, AudioManager.OnAudioFocusChangeListener {
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    @Override
-    public void onAudioFocusChange(int focusChange) {
-
-    }
+public class MusicPlayer extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
+        AudioManager.OnAudioFocusChangeListener, MediaPlayer.OnErrorListener {
 
     public enum PlayerState {
         ERROR,
@@ -36,53 +25,47 @@ public class MusicPlayer extends Service implements MediaPlayer.OnCompletionList
     private List<Track> trackList = null;
     private Integer currentTrackPos = null;
     private PlayerState playerState = PlayerState.NOT_INITIALISED;
-    private void setUpMediaPlayer()
-    {
-        if(this.mediaPlayer == null)
-        {
+
+    private void setUpMediaPlayer() {
+        if(this.mediaPlayer == null) {
             this.mediaPlayer = new MediaPlayer();
+            this.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            this.mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+            this.mediaPlayer.setOnPreparedListener(this);
+            this.mediaPlayer.setOnCompletionListener(this);
+            this.mediaPlayer.setOnErrorListener(this);
         }
-        //TODO: set up player properties, e.g. WakeMode
         changePlayerState(PlayerState.NOT_INITIALISED);
         this.mediaPlayer.reset();
-        this.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        this.mediaPlayer.setOnPreparedListener(this);
-        this.mediaPlayer.setOnCompletionListener(this);
     }
 
-    public void onPlayRequest()
-    {
+    public void onPlayRequest() {
         changePlayerState(PlayerState.PLAYING);
         mediaPlayer.start();
     }
 
-    public void onPauseRequest()
-    {
+    public void onPauseRequest() {
         changePlayerState(PlayerState.PAUSED);
         mediaPlayer.pause();
     }
 
-    public void onNextRequest()
-    {
+    public void onNextRequest() {
         playSongAt(++currentTrackPos);
     }
 
-    public void onStopRequest()
-    {
+    public void onStopRequest() {
         changePlayerState(PlayerState.STOPPED);
         mediaPlayer.stop();
     }
 
-    public void onPrevRequest()
-    {
+    public void onPrevRequest() {
         playSongAt(--currentTrackPos);
     }
 
-    public void playSongAt(int index)
-    {
-        if(index > trackList.size() - 1)
-        {
-            //TODO: OUT OF RANGE
+    public void playSongAt(int index) {
+        if(index > trackList.size() - 1) {
+            changePlayerState(PlayerState.STOPPED);
+            //TODO: notify that playlist is out of songs to play
         }
         try {
             setUpMediaPlayer();
@@ -90,15 +73,13 @@ public class MusicPlayer extends Service implements MediaPlayer.OnCompletionList
             mediaPlayer.setDataSource(trackList.get(currentTrackPos).getStreamUrl());
             changePlayerState(PlayerState.PREPARING);
             mediaPlayer.prepareAsync();
-        } catch (IOException ioExc)
-        {
+        } catch (IOException ioExc) {
             changePlayerState(PlayerState.ERROR);
             //TODO: notify about io error
         }
     }
 
-    private void changePlayerState(PlayerState newState)
-    {
+    private void changePlayerState(PlayerState newState) {
         //TODO: notify about changed state
         this.playerState = newState;
     }
@@ -108,9 +89,7 @@ public class MusicPlayer extends Service implements MediaPlayer.OnCompletionList
         playSongAt(0);
     }
 
-
-    private void setTrackList(List<Track> trackList)
-    {
+    private void setTrackList(List<Track> trackList) {
         this.trackList = trackList;
         this.currentTrackPos = 0;
         setUpMediaPlayer();
@@ -120,15 +99,34 @@ public class MusicPlayer extends Service implements MediaPlayer.OnCompletionList
         changePlayerState(PlayerState.PLAYING);
         mediaPlayer.start();
     }
-
     @Override
     public void onCompletion(MediaPlayer mp) {
         onNextRequest();
     }
-
     @Override
     public void onPrepared(MediaPlayer mp) {
         startPlayng();
     }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public void onAudioFocusChange(int focusChange) {
 
+    }
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        changePlayerState(PlayerState.ERROR);
+        //return value determines if error was handled. Returning false will couse OnCompletionListener
+        //to be called
+        //TODO: show error msg
+        return true;
+    }
+    @Override
+    public void onDestroy() {
+        if(mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+    }
 }
