e9305b66df7ed34edb4fb8875874a500731de0b3
nfetissow <nfetissow@gmail.com>
2016-04-25 21:22:11 +0300

    Json parser unit tests (#40)
    * Parse single Singer test
    
    Positive unit test. Checking if SinlgeSingerParse is working
    
    * Added crucial option
    
    It have to be that way if u want ur unitTests to work
    
    * SingleSingerTest polish
    
    * Fixed error is assert
    
    Checked wrong values
    
    * TrackPasrer UnitTest, SingerParserTest tweaks
    
    Added TrackParserTest,
    Added "Assert not null" in SingerTest, removed "" from ID in json
    string.
    
    * Another error in JSON string for SIngerTest
    
    * Added very important flag
    
    * Update TrackJSONParseTest.java
    
    * Update TrackJSONParseTest.java
    
    * #15 Fix
    
    Test fixed

---
+++ app/src/test/java/ru/bmstu/wundermusik/SingerJSONParseTest.java
@@ -0,0 +1,32 @@
+package ru.bmstu.wundermusik;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+import ru.bmstu.wundermusik.models.Singer;
+import ru.bmstu.wundermusik.models.parsers.*;
+
+
+/**
+ * Created by Nikita on 4/18/2016.
+ */
+public class SingerJSONParseTest {
+    private final String SINGLE_SINGER_JSON = "{\"id\": 359,  \"username\": \"Singer name\",  \"avatar_url\": \"http://www.avatar.url.com/avatar.png\"}";
+    private final String SINGLE_SINGER_AVATAR_URL ="http://www.avatar.url.com/avatar.png";
+    private final long SINGLE_SINGER_ID = 359;
+    private final String SINGLE_SINGER_NAME ="Singer name";
+
+    @Test
+    public void CreateNewSingerTest() {
+        JsonParser<Singer> singerJsonParser = new SingerJsonParser();
+        Singer singer = singerJsonParser.parseSingleObject(SINGLE_SINGER_JSON);
+        assertNotNull(singer);
+        assertEquals(SINGLE_SINGER_AVATAR_URL, singer.getAvatarUrl());
+        assertEquals(SINGLE_SINGER_ID, singer.getId());
+        assertEquals(SINGLE_SINGER_NAME, singer.getName());
+    }
+}
---
+++ app/src/test/java/ru/bmstu/wundermusik/TrackJSONParseTest.java
@@ -0,0 +1,64 @@
+package ru.bmstu.wundermusik;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.Ignore;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+import ru.bmstu.wundermusik.models.Singer;
+import ru.bmstu.wundermusik.models.Track;
+import ru.bmstu.wundermusik.models.parsers.*;
+
+
+/**
+ * Created by Nikita on 4/19/2016.
+ */
+public class TrackJSONParseTest {
+    private final String SINGLE_TRACK_JSON = "{\n" +
+            "  \"streamable\": true,\n" +
+            "  \"original_content_size\": 1034274,\n" +
+            "  \"original_format\": \"mp3\",\n" +
+            "  \"user\": {\n" +
+            "    \"avatar_url\": \"https://i1.sndcdn.com/avatars-000063045564-sauf8o-large.jpg\",\n" +
+            "    \"id\": 40592234,\n" +
+            "    \"kind\": \"user\",\n" +
+            "    \"permalink_url\": \"http://soundcloud.com/shimaa-sami\",\n" +
+            "    \"uri\": \"https://api.soundcloud.com/users/40592234\",\n" +
+            "    \"username\": \"Shimaa Sami\",\n" +
+            "    \"permalink\": \"shimaa-sami\",\n" +
+            "    \"last_modified\": \"2015/10/09 11:16:04 +0000\"\n" +
+            "  },\n" +
+            "  \"id\": 90771495,\n" +
+            "  \"title\": \"Song Title\",\n" +
+            "  \"duration\": 129265,\n" +
+            "  \"stream_url\": \"https://api.soundcloud.com/tracks/90771495/stream\"\n" +
+            "}";
+
+    private final long SINGLE_TRACK_ID = 90771495;
+    private final String SINGLE_TRACK_TITLE = "Song Title";
+    private final long SINGLE_TRACK_DURATION = 129265;
+    private final String SINGLE_TRACK_FORMAT = "mp3";
+    private final long SINGLE_TRACK_CONTENT_SIZE = 1034274;
+    private final String SINGLE_TRACK_STREAM_URL = "https://api.soundcloud.com/tracks/90771495/stream?client_id=114d53b3346a1205caad92e6a74a9214";
+    //private final Singer SINGLE_TRACK_SINGER = null;
+
+
+    @Test
+    public void CreateNewTrackTest() {
+        JsonParser<Track> trackJsonParser = new TrackJsonParser();
+        Track track = trackJsonParser.parseSingleObject(SINGLE_TRACK_JSON);
+        assertNotNull(track);
+        assertEquals(SINGLE_TRACK_ID, track.getId());
+        assertEquals(SINGLE_TRACK_TITLE, track.getTitle());
+        assertEquals(SINGLE_TRACK_DURATION, track.getDuration());
+        assertEquals(SINGLE_TRACK_FORMAT, track.getFormat());
+        assertEquals(SINGLE_TRACK_CONTENT_SIZE, track.getContentSize());
+        assertEquals(SINGLE_TRACK_STREAM_URL, track.getStreamUrl());
+        //assertEquals(SINGLE_TRACK_SINGER, track.getId());
+
+    }
+
+
+}
